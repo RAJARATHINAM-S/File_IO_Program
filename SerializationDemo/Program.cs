bcb13907a35b02a237e2c2f917dd91a1dd8dbcbf
RAJARATHINAM-S <rajarathinams.mech@gmail.com>
2021-10-22 08:12:06 +0530

    Add project files.

---
+++ SerializationDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SerializationDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //BinarySerialization obj = new BinarySerialization();
+           // obj.Serialization();
+            BinaryDeSerialization obj = new BinaryDeSerialization();
+            obj.DeSerialization();
+        }
+    }
+}
